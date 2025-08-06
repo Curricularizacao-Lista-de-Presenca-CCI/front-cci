d87c972bef04232dbb8d2828dcb8d45457c4aaa4
ClaraJoaquim <clarajpmarques@gmail.com>
2025-08-05 22:45:25 -0300

    feat: adicionar tela de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lista-de-presenca';
+
+  constructor(public router: Router){}
 }
