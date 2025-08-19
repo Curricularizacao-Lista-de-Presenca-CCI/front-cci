6b6e59bac89d05f0e237a30402f4b045f0318874
ClaraJoaquim <clarajpmarques@gmail.com>
2025-08-18 21:53:46 -0300

    feat: adicionar componente de login

---
+++ src/app/shared/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterOutlet, RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
