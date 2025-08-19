f99b0739be12a01fae9c70acd97793bf61686a2b
ClaraJoaquim <clarajpmarques@gmail.com>
2025-08-18 22:02:47 -0300

    feat: adicionar componente da tela de lista geral de chamada

---
+++ src/app/shared/lista-geral-presenca/lista-geral-presenca.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-lista-geral-presenca',
+  imports: [NavbarComponent],
+  templateUrl: './lista-geral-presenca.component.html',
+  styleUrl: './lista-geral-presenca.component.css'
+})
+export class ListaGeralPresencaComponent {
+
+}
