4dce7abccd12382bc3375b06eb8dfefd12828bed
ClaraJoaquim <clarajpmarques@gmail.com>
2025-08-18 21:59:14 -0300

    feat: adicionar componente da tela de buscar presença

---
+++ src/app/shared/busca-presenca/busca-presenca.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busca-presenca',
+  imports: [],
+  templateUrl: './busca-presenca.component.html',
+  styleUrl: './busca-presenca.component.css'
+})
+export class BuscaPresencaComponent {
+
+}
