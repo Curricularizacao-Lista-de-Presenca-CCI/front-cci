2b4b6bf8498ac0f50687af9bc1cdf0dab47550e8
ClaraJoaquim <clarajpmarques@gmail.com>
2025-10-20 20:22:16 -0300

    feat: adicionar componente de importar chamada

---
+++ src/app/admin/dialog-importe-chamada/dialog-importe-chamada.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-importe-chamada',
+  imports: [],
+  templateUrl: './dialog-importe-chamada.component.html',
+  styleUrl: './dialog-importe-chamada.component.css'
+})
+export class DialogImporteChamadaComponent {
+
+}
--- src/app/admin/importe-chamada/importe-chamada.component.ts
+++ src/app/admin/importe-chamada/importe-chamada.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { DialogImporteChamadaComponent } from '../dialog-importe-chamada/dialog-importe-chamada.component';
 
 @Component({
   selector: 'app-importe-chamada',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, DialogImporteChamadaComponent ],
   templateUrl: './importe-chamada.component.html',
   styleUrl: './importe-chamada.component.css'
 })
