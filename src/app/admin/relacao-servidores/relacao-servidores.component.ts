1872a938f757121b98abe5aaa25e48897c377459
ClaraJoaquim <clarajpmarques@gmail.com>
2025-10-29 13:51:17 -0300

    feqat: adicionar service de relacao de servidores

--- src/app/admin/relacao-servidores/relacao-servidores.component.ts
+++ src/app/admin/relacao-servidores/relacao-servidores.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { RouterOutlet, RouterModule, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Atuacao } from '../../shared/models/atuacao';
+import { Funcionario } from '../../shared/models/funcionario';
+import { FuncionarioForm } from '../../shared/models/funcionario-form';
+import { RegistroServiceService } from '../registro/service/registro-service.service';
 
 @Component({
   selector: 'app-relacao-servidores',
@@ -9,5 +14,31 @@ import { RouterOutlet, RouterModule } from '@angular/router';
   styleUrl: './relacao-servidores.component.css'
 })
 export class RelacaoServidoresComponent {
+
+  cadastroForm!: FormGroup;
+  cadastrou: boolean = false;
+
+  constructor(
+    private relacaoServidoresService: RelacaoServidoresComponent,
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+  }
+
+
+// buscarListaFuncionarios() {
+
+//   this.relacaoServidoresService.buscarListaFuncionarios(funcionarioParaEnviar).subscribe({
+//     next: () => {
+//       alert('CADASTRO REALIZADO COM SUCESSO!');
+//       this.cadastroForm.reset();
+//     },
+//     error: (erro) => {
+//       console.error('ERRO:', erro);
+//     }
+//   });
+// } 
 
 }
---
+++ src/app/shared/models/uuid.ts
@@ -0,0 +1,2 @@
+export interface Uuid {
+}
