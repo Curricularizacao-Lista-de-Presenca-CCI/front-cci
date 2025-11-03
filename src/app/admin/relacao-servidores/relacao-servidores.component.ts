ee14ab67186f31e578f3c4bae75b27dabb5ee532
ClaraJoaquim <clarajpmarques@gmail.com>
2025-11-03 15:31:28 -0300

    refactor: deletar código desnecessário

--- src/app/admin/relacao-servidores/relacao-servidores.component.ts
+++ src/app/admin/relacao-servidores/relacao-servidores.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { RouterOutlet, RouterModule, Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Atuacao } from '../../shared/models/atuacao';
-import { Funcionario } from '../../shared/models/funcionario';
-import { FuncionarioForm } from '../../shared/models/funcionario-form';
-import { RegistroServiceService } from '../registro/service/registro-service.service';
 
 @Component({
   selector: 'app-relacao-servidores',
@@ -15,30 +10,11 @@ import { RegistroServiceService } from '../registro/service/registro-service.ser
 })
 export class RelacaoServidoresComponent {
 
-  cadastroForm!: FormGroup;
-  cadastrou: boolean = false;
 
   constructor(
-    private relacaoServidoresService: RelacaoServidoresComponent,
-    private formBuilder: FormBuilder,
-    private router: Router,
   ) { }
 
   ngOnInit() {
   }
-
-
-// buscarListaFuncionarios() {
-
-//   this.relacaoServidoresService.buscarListaFuncionarios(funcionarioParaEnviar).subscribe({
-//     next: () => {
-//       alert('CADASTRO REALIZADO COM SUCESSO!');
-//       this.cadastroForm.reset();
-//     },
-//     error: (erro) => {
-//       console.error('ERRO:', erro);
-//     }
-//   });
-// } 
 
 }
