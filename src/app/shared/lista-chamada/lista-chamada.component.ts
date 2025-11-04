40f6fa968ba26be6b5e77043f716c193fbd748cb
ClaraJoaquim <clarajpmarques@gmail.com>
2025-11-04 15:48:19 -0300

    feat: adicionar botão de importar chamada

--- src/app/shared/lista-chamada/lista-chamada.component.ts
+++ src/app/shared/lista-chamada/lista-chamada.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { DialogImporteChamadaComponent } from '../../admin/dialog-importe-chamada/dialog-importe-chamada.component';
 
 @Component({
   selector: 'app-lista-chamada',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, DialogImporteChamadaComponent],
   templateUrl: './lista-chamada.component.html',
   styleUrl: './lista-chamada.component.css'
 })
 export class ListaChamadaComponent {
+
+  
 
 }
