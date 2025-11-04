eedc86dd574abbec3b1a68f908670e193fde3c23
ClaraJoaquim <clarajpmarques@gmail.com>
2025-11-04 15:46:24 -0300

    feat: adicionar service logout

--- src/app/shared/login/service/login.service.ts
+++ src/app/shared/login/service/login.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
-import { Funcionario } from '../../models/funcionario';
+import { Router } from '@angular/router';
+import { TokenService } from '../../service/token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,23 @@ export class LoginService {
   public baseApiUrl = "http://localhost:8080/funcionario";
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router,
+    private tokenService: TokenService
   ) { }
 
-  public login(dadosLogin: any): Observable<Funcionario> {
-    return this.http.post<Funcionario>(`${this.baseApiUrl}/login`, dadosLogin);
+  public login(dadosLogin: any): Observable<any> {
+    return this.http.post<any>(`${this.baseApiUrl}/login`, dadosLogin)
+      .pipe(
+        tap(response => {
+          const token = response.token; 
+
+          if (token) {
+            localStorage.setItem("token", token);
+            this.tokenService.carregarToken();
+            this.router.navigate(['/']);
+          }
+        })
+      );
   }
 }
