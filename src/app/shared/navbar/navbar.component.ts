70f89efd89febfbfe1118bfda4aeae78a2eb5197
ClaraJoaquim <clarajpmarques@gmail.com>
2025-11-04 16:44:15 -0300

    fix: corrigir função duplicada

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { TokenService } from '../service/token.service';
