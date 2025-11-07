67fbd3f76b52d7078712e33ccdf80f56f3fcec29
ClaraJoaquim <clarajpmarques@gmail.com>
2025-11-07 12:22:01 -0300

    fix: corrigir navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { TokenService } from '../service/token.service';
+import Modal from 'bootstrap/js/dist/modal';
 
 @Component({
   selector: 'app-navbar',
@@ -13,6 +14,10 @@ import { TokenService } from '../service/token.service';
 export class NavbarComponent {
   menuAtivo = false;
 
+  @ViewChild('dialogConfirmacaoRef') dialogConfirmarPresencaEl!: ElementRef;
+  private modalConfirmacao: Modal | undefined;
+  private _dialogConfirmacao: boolean = false;
+
   constructor(private tokenService: TokenService) {}
 
   toggleMenu() {
@@ -22,8 +27,32 @@ export class NavbarComponent {
   fecharMenu() {
     this.menuAtivo = false;
   }
+
+  ngAfterViewInit(): void {
+        if (this.dialogConfirmarPresencaEl) {
+          this.modalConfirmacao = new Modal(this.dialogConfirmarPresencaEl.nativeElement);
+        }
+    }
+    
+    set dialogConfirmacao(value: boolean) {
+        this._dialogConfirmacao = value;
+        if (value) {
+          this.modalConfirmacao?.show();
+        } else {
+          this.modalConfirmacao?.hide();
+        }
+    }
   
+    get dialogConfirmacao(): boolean {
+      return this._dialogConfirmacao;
+    }
+  
+    abrirModalConfirmacao() {
+      this.dialogConfirmacao = true;
+    }
+
   fazerLogout(): void {
+    this.dialogConfirmacao = false;
     this.tokenService.logout();
   }
 }
