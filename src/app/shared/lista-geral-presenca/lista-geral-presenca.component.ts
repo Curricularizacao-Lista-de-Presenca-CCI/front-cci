707ae1bbfdc61de12420a199827edb336905de2d
ClaraJoaquim <clarajpmarques@gmail.com>
2025-11-09 14:10:06 -0300

    feat: adicionar edição de presença aos alunos

--- src/app/shared/lista-geral-presenca/lista-geral-presenca.component.ts
+++ src/app/shared/lista-geral-presenca/lista-geral-presenca.component.ts
@@ -1,99 +1,248 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ListaGeralPresencaService } from './service/lista-geral-presenca.service';
 import Toast from 'bootstrap/js/dist/toast';
 import Modal from 'bootstrap/js/dist/modal';
 import { ListaPresencaDTO } from '../models/lista-presenca-dto';
 import { DatePipe, NgFor, NgForOf, NgIf } from '@angular/common';
+import { Evento } from '../models/evento';
+import { BuscarEventosCadastradoDTO } from '../models/buscar-eventos-cadastrado-dto';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FuncionariosAtivosDTO } from '../models/funcionarios-ativos-dto';
+import { ListaChamadaService } from '../lista-chamada/service/lista-chamada.service';
+import { BuscaPresencaService } from '../busca-presenca/service/busca-presenca.service';
+import { ColocarPresenca } from '../models/colocar-presenca';
 
 @Component({
   selector: 'app-lista-geral-presenca',
-  imports: [NavbarComponent, RouterModule, DatePipe, NgForOf, NgFor, NgIf],
+  imports: [NavbarComponent, RouterModule, DatePipe, NgForOf, NgFor, NgIf, FormsModule, ReactiveFormsModule,],
   templateUrl: './lista-geral-presenca.component.html',
   styleUrl: './lista-geral-presenca.component.css'
 })
 export class ListaGeralPresencaComponent implements OnInit {
 
+  modoEdicaoPresenca: boolean = false;
+  termoBusca: string = '';
+  alunosOriginais: any[] = [];
   funcionarioLogado: any;
-  public idEvento!: number; 
+  public idEvento!: number;
   public alunos: ListaPresencaDTO[] = [];
+  public alunosEdicao!: ColocarPresenca;
+  public dadosEvento!: BuscarEventosCadastradoDTO;
+  chamadaForm!: FormGroup;
+  presencaForm!: FormGroup;
+  funcionarios: FuncionariosAtivosDTO[] = [];
+  opcoesAtuacaoFiltrada: any[] = [];
+  opcoesAtuacao: any;
 
-  mensagemErro: string = 'Não foi possível finalizar a chamada. Verifique os dados';
-  mensagemSucesso: string = 'Chamada finalizada com sucesso!';
+  mensagemToast: string = '';
+  toastClass: string = 'text-bg-success';
 
   @ViewChild('dialogEdicaoChamadaRef') modalEdicaoChamadaEl!: ElementRef;
   @ViewChild('dialogFinalizarChamadaRef') modalFinalizarChamadaEl!: ElementRef;
-  @ViewChild('liveToastError') liveToastRefError!: ElementRef;
-  @ViewChild('liveToastSuccess') liveToastRefSuccess!: ElementRef;
+  @ViewChild('dialogDeletarChamadaRef') modalDeletarChamadaEl!: ElementRef;
+  @ViewChild('liveToastGeneric') liveToastGenericRef!: ElementRef;
 
   private modalFinalizarChamada: Modal | undefined;
+  private modalDeletarChamada: Modal | undefined;
   private modalEditarChamada: Modal | undefined;
   private _dialogFinalizarChamada: boolean = false;
+  private _dialogDeletarChamada: boolean = false;
   private _dialogEditarChamada: boolean = false;
 
   constructor(
     private listaGeralService: ListaGeralPresencaService,
-    private route: ActivatedRoute
+    private listaChamadaService: ListaChamadaService,
+    private buscaPresencaService: BuscaPresencaService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+    this.chamadaForm = this.formBuilder.group({
+      local: ['', [Validators.required]],
+      funcionario: [null, [Validators.required]],
+      titulo: [null, [Validators.required]]
+    });
+
+    this.presencaForm = this.formBuilder.group({
+      nomeAluno: [null, [Validators.required]],
+      idEvento: [this.idEvento],
+      idAluno: [null, [Validators.required]]
+    });
+
     const idDaRota = this.route.snapshot.paramMap.get('id');
     if (idDaRota) {
       this.idEvento = Number(idDaRota);
     }
+
+    this.carregarListasDeChamada(this.idEvento);
     this.carregarAlunos(this.idEvento);
     this.carregarDadosLogin();
+    this.carregarFuncionarios();
   }
 
-    ngAfterViewInit(): void {
-      if (this.modalFinalizarChamadaEl) {
-        this.modalFinalizarChamada = new Modal(this.modalFinalizarChamadaEl.nativeElement);
-      }
-
-      if (this.modalEdicaoChamadaEl) {
-        this.modalEditarChamada = new Modal(this.modalEdicaoChamadaEl.nativeElement);
-      }
-    }
-  
-    set dialogFinalizarChamada(value: boolean) {
-      this._dialogFinalizarChamada = value;
-      if (value) {
-        this.modalFinalizarChamada?.show();
-      } else {
-        this.modalFinalizarChamada?.hide();
-      }
-    }
-  
-    get dialogFinalizarChamada(): boolean {
-      return this._dialogFinalizarChamada;
-    }
-  
-    abrirModalChamada() {
-      this.dialogFinalizarChamada = true;
+  ngAfterViewInit(): void {
+    if (this.modalFinalizarChamadaEl) {
+      this.modalFinalizarChamada = new Modal(this.modalFinalizarChamadaEl.nativeElement);
     }
 
-    set dialogEditarChamada(value: boolean) {
-      this._dialogEditarChamada = value;
-      if (value) {
-        this.modalEditarChamada?.show();
-      } else {
-        this.modalEditarChamada?.hide();
+    if (this.modalEdicaoChamadaEl) {
+      this.modalEditarChamada = new Modal(this.modalEdicaoChamadaEl.nativeElement);
+    }
+  }
+
+  set dialogFinalizarChamada(value: boolean) {
+    this._dialogFinalizarChamada = value;
+    if (value) {
+      this.modalFinalizarChamada?.show();
+    } else {
+      this.modalFinalizarChamada?.hide();
+    }
+  }
+
+  get dialogFinalizarChamada(): boolean {
+    return this._dialogFinalizarChamada;
+  }
+
+  abrirModalChamada() {
+    this.dialogFinalizarChamada = true;
+  }
+
+  set dialogEditarChamada(value: boolean) {
+    this._dialogEditarChamada = value;
+    if (value) {
+      this.modalEditarChamada?.show();
+    } else {
+      this.modalEditarChamada?.hide();
+    }
+  }
+
+  get dialogEditarChamada(): boolean {
+    return this._dialogEditarChamada;
+  }
+
+  abrirModalEditarChamada() {
+    this.dialogEditarChamada = true;
+
+    this.chamadaForm.patchValue({
+      titulo: this.dadosEvento.titulo,
+      local: this.dadosEvento.local,
+      funcionario: this.dadosEvento.idFuncionario
+    });
+
+    this.opcoesAtuacaoFiltrada = this.opcoesAtuacao.filter(
+      (area: FuncionariosAtivosDTO) => area.nome !== this.dadosEvento.funcionario
+    );
+  }
+
+  set dialogDeletarChamada(value: boolean) {
+    this._dialogDeletarChamada = value;
+    if (value) {
+      this.modalDeletarChamada?.show();
+    } else {
+      this.modalDeletarChamada?.hide();
+    }
+  }
+
+  get dialogDeletarChamada(): boolean {
+    return this._dialogDeletarChamada;
+  }
+
+  abrirModalDeletarChamada() {
+    this.dialogFinalizarChamada = true;
+  }
+
+  carregarFuncionarios() {
+    this.listaChamadaService.buscarFuncionariosAtivos().subscribe({
+      next: (dados: FuncionariosAtivosDTO[]) => {
+        this.funcionarios = dados;
+        this.opcoesAtuacao = this.funcionarios;
+      },
+      error: (err) => {
+        console.error('Erro ao buscar funcionários', err);
       }
+    });
+  }
+
+  carregarAlunos(idEvento: number) {
+    this.listaGeralService.buscarAlunos(idEvento).subscribe({
+      next: (dados: ListaPresencaDTO[]) => {
+        this.alunos = dados;
+        console.log(this.alunos);
+      },
+      error: (err) => {
+        console.error('Erro ao buscar listas', err);
+      }
+    });
+  }
+
+  carregarListasDeChamada(idEvento: number) {
+    this.listaGeralService.buscarChamada(idEvento).subscribe({
+      next: (dados: BuscarEventosCadastradoDTO) => {
+        this.dadosEvento = dados;
+        console.log(this.dadosEvento);
+      },
+      error: (err) => {
+        console.error('Erro ao buscar listas', err);
+      }
+    });
+  }
+
+  carregarDadosLogin() {
+    const funcionarioString = localStorage.getItem("funcionario");
+
+    if (funcionarioString) {
+      try {
+        this.funcionarioLogado = JSON.parse(funcionarioString);
+        console.log('Dados do Funcionário Logado:', this.funcionarioLogado);
+
+      } catch (e) {
+        console.error("Erro ao analisar dados do funcionário no localStorage:", e);
+        localStorage.removeItem("funcionario");
+      }
+    } else {
+      console.warn("Nenhuma informação de funcionário encontrada no localStorage.");
     }
-  
-    get dialogEditarChamada(): boolean {
-      return this._dialogEditarChamada;
+  }
+
+  public EditarChamada(): void {
+
+    const toast = new Toast(this.liveToastGenericRef.nativeElement);
+
+    if (!this.idEvento) {
+      console.error('ID do evento não encontrado!');
+      return;
     }
-  
-    abrirModalEditarChamada() {
-      this.dialogEditarChamada = true;
-    }
+
+    const dadosAlterados = {
+      ...this.dadosEvento,
+      local: this.chamadaForm.get('local')?.value,
+      funcionario: this.chamadaForm.get('funcionario')?.value,
+      titulo: this.chamadaForm.get('titulo')?.value,
+    };
+
+    this.listaGeralService.alterarChamada(dadosAlterados, this.idEvento).subscribe({
+      next: () => {
+        this.mensagemToast = 'Chamada editada com sucesso!';
+        toast.show();
+        this.carregarListasDeChamada(this.idEvento);
+        this.dialogEditarChamada = false;
+        console.log('Chamada editada com sucesso!');
+      },
+      error: (err) => {
+        this.mensagemToast = 'Não foi possível editar a chamada. Verifique os dados';
+        this.toastClass = 'text-bg-danger';
+        toast.show();
+        console.error('Erro ao finalizar chamada:', err);
+      }
+    });
+  }
 
   public finalizarChamada(): void {
-
-    const toastSucess = new Toast(this.liveToastRefSuccess.nativeElement);
-    const toastError = new Toast(this.liveToastRefError.nativeElement);
+    const toast = new Toast(this.liveToastGenericRef.nativeElement);
 
     if (!this.idEvento) {
       console.error('ID do evento não encontrado!');
@@ -103,43 +252,76 @@ export class ListaGeralPresencaComponent implements OnInit {
     this.listaGeralService.finalizarChamada(this.idEvento).subscribe({
       next: () => {
         this.dialogFinalizarChamada = false;
-        toastSucess.show();
-        console.log('Chamada finalizada com sucesso!');
+        this.mensagemToast = 'Chamada finalizada com sucesso!';
+        this.router.navigate(['/lista-chamada']);
+        toast.show();
       },
       error: (err) => {
-        toastError.show();
+        this.mensagemToast = 'Não foi possível finalizar a chamada. Verifique os dados';
+        this.toastClass = 'text-bg-danger';
+        toast.show();
         console.error('Erro ao finalizar chamada:', err);
       }
     });
   }
 
-    carregarAlunos(idEvento: number) {
-      this.listaGeralService.buscarAlunos(idEvento).subscribe({
-        next: (dados: ListaPresencaDTO[]) => {
-          this.alunos = dados;
-          console.log(this.alunos);
-        },
-        error: (err) => {
-          console.error('Erro ao buscar listas', err);
-        }
-      });
+
+  prepararEdicao() {
+    this.alunosOriginais = JSON.parse(JSON.stringify(this.alunos));
+    this.modoEdicaoPresenca = true;
+  }
+
+  atualizarStatusPresenca(aluno: any, event: Event): void {
+    const toast = new Toast(this.liveToastGenericRef.nativeElement);
+
+    const target = event.target as HTMLInputElement;
+
+    aluno.status = target.checked ? 'Sim' : 'Não';
+
+    const dadosPresenca: ColocarPresenca = {
+      nomeAluno: aluno.nomeAluno,
+      idEvento: this.idEvento,
+      idAluno: aluno.idAluno
+    };
+
+    this.buscaPresencaService.registrarPresenca(dadosPresenca).subscribe({
+      next: (response) => {
+        this.mensagemToast = aluno.status === 'Sim'
+          ? `Presença de ${aluno.nomeAluno} marcada!`
+          : `Presença de ${aluno.nomeAluno} removida!`;
+        this.toastClass = 'text-bg-warning';
+        toast.show();
+      },
+      error: (err) => {
+        this.mensagemToast = `Erro ao atualizar presença de ${aluno.nomeAluno}.`;
+        this.toastClass = 'text-bg-danger';
+        toast.show();
+        aluno.status = target.checked ? 'Não' : 'Sim';
+      }
+    });
+  }
+
+  salvarPresencasEditadas() {
+    this.modoEdicaoPresenca = false;
+  }
+
+  carregarAlunosOriginal() {
+    this.modoEdicaoPresenca = false;
+    this.carregarAlunos(this.idEvento);
+  }
+
+  get alunosFiltrados(): ListaPresencaDTO[] {
+    if (!this.termoBusca) {
+        return this.alunos; // Retorna a lista completa se não houver termo
     }
 
-    carregarDadosLogin() {
-    const funcionarioString = localStorage.getItem("funcionario");
-    
-    if (funcionarioString) {
-      try {
-        this.funcionarioLogado = JSON.parse(funcionarioString);
-        console.log('Dados do Funcionário Logado:', this.funcionarioLogado);
-        
-      } catch (e) {
-        console.error("Erro ao analisar dados do funcionário no localStorage:", e);
-        localStorage.removeItem("funcionario");
-      }
-    } else {
-      console.warn("Nenhuma informação de funcionário encontrada no localStorage.");
-    }
-  }
+    const termoBuscaLower = this.termoBusca.toLowerCase();
+
+    // Filtra a lista de alunos pelo campo 'nomeAluno'
+    return this.alunos.filter(aluno => {
+        // Assume que ListaPresencaDTO tem a propriedade 'nomeAluno'
+        return aluno.nomeAluno.toLowerCase().includes(termoBuscaLower);
+    });
+}
 
 }
--- src/app/shared/lista-geral-presenca/service/lista-geral-presenca.service.ts
+++ src/app/shared/lista-geral-presenca/service/lista-geral-presenca.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListaPresencaDTO } from '../../models/lista-presenca-dto';
+import { Evento } from '../../models/evento';
+import { BuscarEventosCadastradoDTO } from '../../models/buscar-eventos-cadastrado-dto';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +17,10 @@ export class ListaGeralPresencaService {
     private http: HttpClient
   ) { }
 
+  public buscarChamada(idEvento: number): Observable<BuscarEventosCadastradoDTO> {
+    return this.http.get<BuscarEventosCadastradoDTO>(`${this.baseApiUrlEvento}/buscar-informacao-chamada/${idEvento}`);
+  }
+
   public finalizarChamada(idEvento: number): Observable<void> {
     return this.http.post<void>(`${this.baseApiUrlEvento}/finalizar-chamada/${idEvento}`, null);
   }
@@ -23,12 +29,7 @@ export class ListaGeralPresencaService {
     return this.http.get<ListaPresencaDTO[]>(`${this.baseApiUrlListaPresenca}/buscar-todos-alunos/${idFuncionario}`);
   }
 
-  public alterarChamada(evento: ListaPresencaDTO,idEvento: number): Observable<ListaPresencaDTO[]> {
-    return this.http.post<ListaPresencaDTO[]>(`${this.baseApiUrlEvento}/alterar-lista-chamada/${idEvento}`, evento);
-  }
-
-    
-public RemoverChamada(idEvento: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseApiUrlEvento}/remover-lista-chamada/${idEvento}`);
+  public alterarChamada(evento: BuscarEventosCadastradoDTO, idEvento: number): Observable<BuscarEventosCadastradoDTO> {
+    return this.http.post<BuscarEventosCadastradoDTO>(`${this.baseApiUrlEvento}/alterar-lista-chamada/${idEvento}`, evento);
   }
 }
