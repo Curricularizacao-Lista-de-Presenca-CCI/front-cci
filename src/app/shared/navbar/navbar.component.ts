22e87f0b19c2bcca4df78951cd20894c732261fc
ClaraJoaquim <clarajpmarques@gmail.com>
2025-11-09 14:07:37 -0300

    refactor: corrigir botão do navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { TokenService } from '../service/token.service';
@@ -11,14 +11,21 @@ import Modal from 'bootstrap/js/dist/modal';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   menuAtivo = false;
+  funcionarioLogado: any;
 
   @ViewChild('dialogConfirmacaoRef') dialogConfirmarPresencaEl!: ElementRef;
   private modalConfirmacao: Modal | undefined;
   private _dialogConfirmacao: boolean = false;
 
   constructor(private tokenService: TokenService) {}
+
+
+  ngOnInit(): void {
+    this.carregarDadosLogin();
+  }
+  
 
   toggleMenu() {
     this.menuAtivo = !this.menuAtivo;
@@ -54,5 +61,22 @@ export class NavbarComponent {
   fazerLogout(): void {
     this.dialogConfirmacao = false;
     this.tokenService.logout();
+  }
+
+  carregarDadosLogin() {
+    const funcionarioString = localStorage.getItem("funcionario");
+    
+    if (funcionarioString) {
+      try {
+        this.funcionarioLogado = JSON.parse(funcionarioString);
+        console.log('Dados do Funcionário Logado:', this.funcionarioLogado);
+        
+      } catch (e) {
+        console.error("Erro ao analisar dados do funcionário no localStorage:", e);
+        localStorage.removeItem("funcionario");
+      }
+    } else {
+      console.warn("Nenhuma informação de funcionário encontrada no localStorage.");
+    }
   }
 }
